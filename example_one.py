eff2519a30b93c15d5a250b59d8776d4f808d7a2
Bhavan-Techlabs <91679346+Bhavan-Techlabs@users.noreply.github.com>
2025-10-04 12:32:12 +0000

    Update page icons and improve navigation handling in Streamlit app

--- example_one.py
+++ example_one.py
@@ -5,9 +5,11 @@ import streamlit as st
 from st_pages import add_page_title, get_nav_from_toml
 
 st.write("`streamlit_app.py` contents:")
-code = Path("example_app/streamlit_app.py").read_text()
+code = Path("./streamlit_app.py").read_text()
 
 st.code(code, language="python")
+
+st.session_state["use_sections"] = st.checkbox("Use sections", value=True)
 
 location = "pages_sections.toml" if st.session_state["use_sections"] else "pages.toml"
 st.write(f"`{location}` contents:")
--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,12 @@
 import streamlit as st
-
 from st_pages import add_page_title, get_nav_from_toml
 
 st.set_page_config(layout="wide")
 
-sections = st.sidebar.toggle("Sections", value=True, key="use_sections")
-
-nav = get_nav_from_toml(
-    ".streamlit/pages_sections.toml" if sections else ".streamlit/pages.toml"
-)
-
-st.logo("logo.png")
+# If you want to use the no-sections version, this
+# defaults to looking in .streamlit/pages.toml, so you can
+# just call `get_nav_from_toml()`
+nav = get_nav_from_toml(".streamlit/pages_sections.toml")
 
 pg = st.navigation(nav)
 
